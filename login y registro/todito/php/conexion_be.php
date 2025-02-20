1c695785109fe21abbf4724598e0098a3ffba193
1002969024jhon <jhonmanquillo@unicomfacauca.edu.co>
2025-02-20 09:12:30 -0500

    Add files via upload

---
+++ login y registro/todito/php/conexion_be.php
@@ -0,0 +1,11 @@
+<?php  
+$conn = mysqli_connect("localhost", "root", "", "login_register_db")
+
+/*if ($conn->connect_errno) {
+    die("Conexión fallida: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa";
+
+}*/
+?>
+
